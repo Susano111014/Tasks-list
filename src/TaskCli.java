c6daa0a5491582b2140258a04eff9c83cfc49109
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-10-30 21:19:01 -0600

    implement a method which make it a guide of the actions commands to a Tasks in the CLI

--- src/TaskCli.java
+++ src/TaskCli.java
@@ -6,6 +6,21 @@ public class TaskCli {
     Date createdAt;
     Date updatedAt;
     Status status;
+
+    static void tasksCommands() {
+        System.out.println("#Yo can create a new task with 'add <task>'");
+        System.out.println("#Yo can delete tasks with 'delete <task-number>'");
+        System.out.println("#Yo can finishing tasks with 'done <task-number>'");
+        System.out.println("#Yo can update a task with 'update <task-number> <update-task>'");
+        System.out.println("#Yo can mark task as in progress with 'mark-in-progress <task-number>'");
+        System.out.println("You can Listing all tasks with 'list-all'");
+        System.out.println("You can Listing tasks by status with 'list <tasks-status>'");
+    }
+
+    public static void main (String[] input){
+        tasksCommands();
+    }
+
 }
 
 enum Status {
